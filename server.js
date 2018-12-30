9cd02c0cb0c71d3da5b2e34a3ae6683297e84677
Shekhar Goswami - Supplier <sgoswa2@uk.wal-mart.com>
2018-12-30 22:34:30 +0000

    Just for testing purpose

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req,res,next) => {
     console.log(log);
 
     fs.appendFile("server.log",log + '\n',(err) => {
-        if(err){
+        if(error){
             console.log("Error while writing the server log");
         }
     });
