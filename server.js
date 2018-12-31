e07670441ab78032e4947dbbf3be1c4c6c8f857b
Shekhar Goswami <shekhar192002@gmail.com>
2018-12-31 13:38:03 +0000

    Another commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req,res,next) => {
     console.log(log);
 
     fs.appendFile("server.log",log + '\n',(err) => {
-        if(error){
+        if(err){
             console.log("Error while writing the server log");
         }
     });
